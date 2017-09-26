aa949482aee3d20c18f2d8754d97d7a192fade7a
pskushw <pskushw@users.noreply.github.com>
2017-09-27 00:45:28 +0530

    Singleton pattern

---
+++ singletonPattern.js
@@ -0,0 +1,25 @@
+
+var singleton = (function(){
+  var instance;
+  
+  function init(){
+    return {
+      randomNumber : Math.random()
+    }
+  }
+  return {
+    getInstance: function(){
+      if(!instance){
+        instance = init();
+      }
+      return instance;
+    }
+  }
+})();
+
+//check if its singleton
+var instance1 = singleton.getInstance();
+var instance2 = singleton.getInstance();
+if(instance1 === instance2){
+  console.log("its singleton");
+}
