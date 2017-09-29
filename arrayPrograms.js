92f6d889f99d29d72a481ce00317ca6afe45b0f4
pskushw <pskushw@users.noreply.github.com>
2017-09-29 15:55:20 +0530

    Update arrayPrograms.js

--- arrayPrograms.js
+++ arrayPrograms.js
@@ -2,11 +2,20 @@
 A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. For example, if left rotations are performed on array , then the array would become .
 Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.*/
 var a = [1,2,3,4,5]
-
+//by loop
 Array.prototype.rotateArray = function(num) {
   for(var i = 0; i < num ; i++) {
     var removeElement = this.shift();
     this.push(removeElement);
+  }
+  return this;
+}
+//by recursion
+Array.prototype.rotateArray = function(num) {
+  if(num>0){
+    var removedElement = this.shift();
+    this.push(removedElement);
+    this.rotateArray(num - 1);
   }
   return this;
 }
