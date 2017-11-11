e3815bedf8816c1da43ac98365222432b6ce3a0f
pskushw <pskushw@users.noreply.github.com>
2017-11-12 02:58:07 +0530

    Separate even and odd integers in a given array

--- sortbooleanArray.js
+++ sortbooleanArray.js
@@ -21,3 +21,31 @@ function sortbooleanArray(a) {
 }
 
 console.log(sortbooleanArray([0,0,1,0,1,1,0,1,0,1,1,0]));
+
+
+//Separate even and odd integers in a given array
+var a = [1,2,3,4,6,8,7,12]
+
+function separateEvenAndOdd(ary) {
+  var x = 0;
+  var y = ary.length - 1;
+  while(x != y){
+    if(ary[x]%2 === 0){
+      x++;
+    }
+    if(ary[x]%2 === 1){
+      if(ary[y]%2 === 1){
+        y--;
+      }else if(ary[y]%2 === 0){
+        var temp = ary[y];
+        ary[y] = ary[x];
+        ary[x] = temp;
+        x++;
+        y--;
+      }
+    }
+  }
+  return ary;
+}
+
+console.log(separateEvenAndOdd(a));
